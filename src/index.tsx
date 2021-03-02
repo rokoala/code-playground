554ae1f9c93d2a9c84feea81b222b893fdb383cb
rokoala <koga.rodrigo@gmail.com>
2021-03-02 17:40:35 -0300

    Add SimpleCode view

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import SimpleCodeView from 'views/SimpleCode';
 import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <SimpleCodeView />
     </React.StrictMode>,
     document.getElementById('root'),
 );
---
+++ src/views/SimpleCode.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { BoxCode } from 'components';
+
+const SimpleCode: React.FC = () => {
+    const [JSCode, setJSCode] = useState('');
+    const [HTMLCode, setHTMLCode] = useState('');
+    const [CSSCode, setCSSCode] = useState('');
+
+    return (
+        <>
+            <div>
+                <BoxCode title="HTML" onCodeChange={(value) => setHTMLCode(value)} />
+                <BoxCode
+                    title="JS"
+                    codeMirrorOptions={{ mode: 'javascript' }}
+                    onCodeChange={(value) => setJSCode(value)}
+                />
+                <BoxCode title="CSS" codeMirrorOptions={{ mode: 'css' }} onCodeChange={(value) => setCSSCode(value)} />
+            </div>
+            <iframe
+                sandbox="allow-downloads allow-forms allow-modals allow-pointer-lock allow-popups allow-presentation allow-same-origin allow-scripts allow-top-navigation-by-user-activation"
+                title="preview"
+                srcDoc={`${HTMLCode} <style>${CSSCode}</style><script>${JSCode}</script>`}
+            />
+        </>
+    );
+};
+
+export default SimpleCode;
