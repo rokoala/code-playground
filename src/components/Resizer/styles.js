b6aebb7f03f2e2393595b183a30799d41f9e246f
rokoala <koga.rodrigo@gmail.com>
2021-03-03 19:15:47 -0300

    Edit resizer css

--- src/components/Resizer/styles.js
+++ src/components/Resizer/styles.js
@@ -3,5 +3,7 @@ import styled from 'styled-components';
 export const Bar = styled.div`
     cursor: col-resize;
     min-width: 18px;
+    border-left: 1px solid rgba(255, 255, 255, 0.05);
+    border-right: 1px solid rgba(0, 0, 0, 0.4);
     background-color: #333642;
 `;
