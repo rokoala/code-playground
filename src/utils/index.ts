b5d84ffa25b4ebf107faace5070ad63168db2cf9
rokoala <koga.rodrigo@gmail.com>
2021-03-03 14:04:42 -0300

    Add windows hook dimensions

---
+++ src/utils/index.ts
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+interface WindowDimensions {
+    width: number;
+    height: number;
+}
+
+const getWindowDimensions = () => {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+        width,
+        height,
+    };
+};
+
+// useWindowDimensions - React hook to get window dimensions
+export const useWindowDimensions = ():WindowDimensions => {
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return windowDimensions;
+};
