768b45e1c9f1f21454e18e47c67c343b777d48b7
rokoala <koga.rodrigo@gmail.com>
2021-03-08 01:43:47 -0300

    Remove BoxCodeBlock

--- src/components/BoxCodeBlock/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const CodeBlock = styled.div`
-    display: flex;
-    position: relative;
-    background-color: black;
-    flex-direction: row;
-    width: 100%;
-`;
